96e6608dc957e3c176b4af2228f5b1be8046b67e
王华 <root1@wanghuadeMBP.mshome.net>
2024-10-22 10:30:19 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-3xl text-blue-500'><h1>Vite + React + TypeScript + Tailwind CSS Example</h1></div>
+  )
+}
+
+export default App
